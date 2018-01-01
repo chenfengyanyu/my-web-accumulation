2408892dc38f5863c9b12b2dca0870c9dfd6f48f
chenfengyanyu <jartto@qq.com>
2018-01-01 14:24:15 +0800

    jartto:test remote

--- electron-quick-start/main.js
+++ electron-quick-start/main.js
@@ -18,7 +18,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'desktopCapturer/index.html'),
+    pathname: path.join(__dirname, 'webFrame/index.html'),
     protocol: 'file:',
     slashes: true
   }))
