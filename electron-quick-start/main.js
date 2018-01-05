95c0b2e7f23180f5c3ccd72cb8775dbf021dcef2
chenfengyanyu <jartto@qq.com>
2018-01-05 11:19:10 +0800

    jartto:update electron

--- electron-quick-start/main.js
+++ electron-quick-start/main.js
@@ -19,7 +19,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'notification/index.html'),
+    pathname: path.join(__dirname, 'webview/index.html'),
     protocol: 'file:',
     slashes: true
   }))
