2f4d8b198680e4c5ba959fbdefe6cc76d224f270
Jartto <jartto@wangjiataodeMacBook-Pro.local>
2018-04-04 18:09:20 +0800

    jartto:add plugins mathjax

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -6,5 +6,7 @@
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+  // config.uiColor = 'red';
+  config.extraPlugins = 'mathjax';
+  config.mathJaxLib = '//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS_HTML';
 };
