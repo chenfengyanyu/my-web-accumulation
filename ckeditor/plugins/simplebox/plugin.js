177bfceed50b7ccca6a5edd07510bd7cbfe66e92
chenfengyanyu <jartto@qq.com>
2018-04-08 16:27:07 +0800

    jartto:update simplebox widget

--- ckeditor/plugins/simplebox/plugin.js
+++ ckeditor/plugins/simplebox/plugin.js
@@ -20,11 +20,40 @@ CKEDITOR.plugins.add('simplebox', {
           allowedContent: 'p br ul ol li strong em'
         }
       },
-      allowedContent: 'div(!simplebox); div(!simplebox-content); h2(!simplebox-title)',
+      allowedContent: 'div(!simplebox,align-left,align-right,align-center){width};' +
+        'div(!simplebox); div(!simplebox-content); h2(!simplebox-title)',
       requiredContent: 'div(simplebox)',
+      dialog: 'simplebox',
       upcast: function (element) {
         return element.name == 'div' && element.hasClass('simplebox');
+      },
+      init: function () {
+        var width = this.element.getStyle('width');
+        if (width)
+          this.setData('width', width);
+        if (this.element.hasClass('align-left'))
+          this.setData('align', 'left');
+        if (this.element.hasClass('align-right'))
+          this.setData('align', 'right');
+        if (this.element.hasClass('align-center'))
+          this.setData('align', 'center');
+      },
+
+      data: function () {
+
+        if (this.data.width == '')
+          this.element.removeStyle('width');
+        else
+          this.element.setStyle('width', this.data.width);
+
+        this.element.removeClass('align-left');
+        this.element.removeClass('align-right');
+        this.element.removeClass('align-center');
+        if (this.data.align)
+          this.element.addClass('align-' + this.data.align);
       }
     });
+
+    CKEDITOR.dialog.add('simplebox', this.path + 'dialogs/simplebox.js');
   }
 });
