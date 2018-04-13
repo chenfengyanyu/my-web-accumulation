720a067b995d4725130a6b8d2019f457b982b1ba
chenfengyanyu <jartto@qq.com>
2018-04-13 14:12:33 +0800

    jartto:add easyimage

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -8,7 +8,10 @@ CKEDITOR.editorConfig = function( config ) {
 	// config.language = 'fr';
   // config.uiColor = '#66AB16';
   // config.readOnly = true;
-  config.extraPlugins = 'mathjax,timestamp,abbr,contextmenu,simplebox,newblod';
+  config.extraPlugins = 'mathjax,timestamp,abbr,contextmenu,simplebox,newblod,easyimage';
   config.mathJaxLib = '//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS_HTML';
   config.extraAllowedContent = 'str[font-weight]';
+  config.cloudServices_tokenUrl = 'https://16319.cke-cs.com/token/dev/PHE4JzvHCrdHd68h1cQWwjHJOyxLlZzA4RzCamQaMYlEHy26GKtXOlwkuRVl',
+  // config.cloudServices_uploadUrl = 'https://16319.cke-cs.com/easyimage/upload/'
+  config.cloudServices_uploadUrl = "http://127.0.0.1:6688/collector/ckeditor_upload"
 };
