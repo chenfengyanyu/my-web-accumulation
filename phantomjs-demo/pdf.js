6cd73245d9ad450154921b054276ecbae0be8870
chenfengyanyu <jartto@qq.com>
2018-11-15 20:30:12 +0800

    add: h5-geolocation

--- phantomjs-demo/pdf.js
+++ phantomjs-demo/pdf.js
@@ -10,11 +10,7 @@ const phantom = require('phantom');
   });
 
 
-<<<<<<< HEAD
-  const status = await page.open('http://shensz-t.test.17zuoye.net/printer/pc/#!/class-report?from=paper-manage&group_id=34490&paper_id=3c5d7c7b-5bde-41b6-8cd2-5a7be5ed2c72&session_key=e55e550587061ddd58035207997b402f');
-=======
-  const status = await page.open('http://shensz-t.test.17zuoye.net/printer/pc/#!/class-report?from=paper-manage&group_id=34548&paper_id=5d4d9dfb-4a7f-49a7-bc20-fd41bad56faa&session_key=ba927753860310643fa421dc8b0fff19');
->>>>>>> 23e3da706856317d0d7d76d1814ca5b1538a5e46
+  const status = await page.open('https://shensz-t.test.17zuoye.net/printer/pc/#!/class-report?from=paper-manage&group_id=34490&paper_id=a6dbefab-7a3e-4758-8476-2a824b9c1c25&session_key=e55e550587061ddd58035207997b402f');
   // const status = await page.open('http://localhost:5002/printer/pc/#!/class-report?from=paper-manage&group_id=34548&paper_id=5d4d9dfb-4a7f-49a7-bc20-fd41bad56faa&session_key=ba927753860310643fa421dc8b0fff19');
 
   await page.render('test2.pdf');
--- puppeteer-test/pdf.js
+++ puppeteer-test/pdf.js
@@ -20,7 +20,7 @@ let { timeout } = require('./tools/tools.js');
   // await page.goto('http://jartto.wang');
   await page.goto(
     // 'https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pdf',
-    'https://shensz-t.test.17zuoye.net/printer/pc/#!/class-report?from=paper-manage&group_id=34490&paper_id=3c5d7c7b-5bde-41b6-8cd2-5a7be5ed2c72&session_key=e55e550587061ddd58035207997b402f',
+    'https://shensz-t.test.17zuoye.net/printer/pc/#!/class-report?from=paper-manage&group_id=34490&paper_id=a6dbefab-7a3e-4758-8476-2a824b9c1c25&session_key=e55e550587061ddd58035207997b402f',
     { waitUntil: ['domcontentloaded','networkidle0']}
   );
   await page.waitFor('*');
