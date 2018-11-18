2ca470e5ec5f4d7a5e47e4420e562de1357bf1ef
chenfengyanyu <jartto@qq.com>
2018-11-18 22:21:35 +0800

    add: algorithm

---
+++ algorithm/binary-search.js
@@ -0,0 +1,26 @@
+/**
+ * @msg: 二分查找
+ * @param {Array} 数组
+ * @param {String} 数值
+ * @return: index
+ */
+const binarySearch = (arr, val) => {
+  let start = 0;
+  let end = arr.length - 1;
+  let guess;
+
+  while (start <= end) {
+    let mid = Math.ceil((start + end) / 2);
+    guess = arr[mid];
+    if (guess === val) 
+      return mid;
+    if (guess > val) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return -1;
+}
+
+binarySearch([1, 3, 5, 7, 9], 3);
