7a0d29b5159781fb21d968579def70011f2fd565
chenfengyanyu <jartto@qq.com>
2018-12-21 10:46:54 +0800

    refactor: web woeker

--- web-worker/src/main.js
+++ web-worker/src/main.js
@@ -1,9 +1,19 @@
-const worker = new Worker("../src/worker.js");
+const worker = new Worker('src/worker.js');
 
 worker.onmessage = e => {
   const message = e.data;
   console.log(`[From Worker]: ${message}`);
+  document.getElementById('app').innerHTML = message;
 };
 
-worker.postMessage("Marco!");
+worker.onerror = function(error) {
+  console.log(error);
+  worker.terminate();
+  // throw error;
+};
+
+setTimeout(()=>{
+  worker.postMessage('写的真好!');
+},3000)
+
 
