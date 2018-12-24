038f3725a3253434af5dddf6f3dd5cb38eb2273a
chenfengyanyu <jartto@qq.com>
2018-12-24 20:51:21 +0800

    feat: add webpack-demo

---
+++ webpack-demo/js/main.js
@@ -0,0 +1,6 @@
+// 通过 CommonJS 规范导入 CSS 模块
+require('../css/main.css');
+// 通过 CommonJS 规范导入 show 函数
+const show = require('./show.js');
+// 执行 show 函数
+show('Webpack');
---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  // JavaScript 执行入口文件
+  entry: './js/main.js',
+  output: {
+    // 把所有依赖的模块合并输出到一个 bundle.js 文件
+    filename: 'bundle.js',
+    // 输出文件都放到 dist 目录下
+    path: path.resolve(__dirname, './dist'),
+  },
+  module: {
+    rules: [
+      {
+        // 用正则去匹配要用该 loader 转换的 CSS 文件
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          // 转换 .css 文件需要使用的 Loader
+          use: ['css-loader'], 
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      // 从 .js 文件中提取出来的 .css 文件的名称
+      filename: `[name]_[hash:8].css`,
+    })
+  ]
+};
