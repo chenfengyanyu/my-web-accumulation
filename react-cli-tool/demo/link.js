24c02335e17387633b2127ed0f29b897a419a798
jartto <jartto@qq.com>
2019-06-14 12:51:21 +0800

    add: link

---
+++ react-cli-tool/demo/link.js
@@ -0,0 +1,11 @@
+// https://github.com/sindresorhus/ink-link
+
+import React from 'react';
+import {render, Color} from 'ink';
+import Link from 'ink-link';
+
+render(
+	<Link url="http://jartto.wang">
+		<Color yellow>Jartto's Blog</Color>
+	</Link>
+);
--- react-cli-tool/index.js
+++ react-cli-tool/index.js
@@ -4,4 +4,4 @@ require("@babel/register")({
     presets: ["@babel/preset-env", "@babel/preset-react"]
 });
 
-require('import-jsx')('./demo/select');
+require('import-jsx')('./demo/link');
