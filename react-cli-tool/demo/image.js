75a3f13916951902df8252e3e2dddad59e019e8e
jartto <jartto@qq.com>
2019-06-15 15:58:44 +0800

    add: img

---
+++ react-cli-tool/demo/image.js
@@ -0,0 +1,9 @@
+// https://github.com/kevva/ink-image
+
+import React from 'react';
+import {render} from 'ink';
+import Image from 'ink-image';
+
+render(
+	<Image preserveAspectRatio src='img/thank.png' width='50%'/>
+);
