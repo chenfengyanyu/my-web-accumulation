fef9f55e2c4fe66a11463a244064fdb11b114e46
jartto <jartto@qq.com>
2019-06-15 19:10:45 +0800

    add: divider

---
+++ react-cli-tool/demo/divider.js
@@ -0,0 +1,8 @@
+// https://github.com/JureSotosek/ink-divider
+
+import React from 'react';
+import {render} from 'ink';
+import Divider from 'ink-divider';
+
+render(<Divider dividerColor="yellow" dividerChar="~" title={`Jartto's Blog`} />);
+
