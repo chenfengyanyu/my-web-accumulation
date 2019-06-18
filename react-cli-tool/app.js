c5c00199eacd5cb29f5cba4fd70eb55bd7dc9937
jartto <jartto@qq.com>
2019-06-18 11:31:18 +0800

    add: ink-figlet

--- react-cli-tool/app.js
+++ react-cli-tool/app.js
@@ -4,4 +4,4 @@ require("@babel/register")({
     presets: ["@babel/preset-env", "@babel/preset-react"]
 });
 
-require('import-jsx')('./demo/animation');
+require('import-jsx')('./demo/figlet');
---
+++ react-cli-tool/demo/figlet.js
@@ -0,0 +1,7 @@
+// https://github.com/KimotoYanke/ink-figlet
+
+import React from 'react';
+import { h, render } from 'ink';
+import Figlet from 'ink-figlet';
+
+render(<Figlet font="Ghost">Hello</Figlet>)
