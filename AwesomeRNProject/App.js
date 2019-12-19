f779f9467ab0d0e7fa60a4d67715d22f92a9e1c0
jartto <jartto@qq.com>
2019-12-19 17:19:48 +0800

    add: electron png

---
+++ AwesomeRNProject/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class HelloWorldApp extends Component {
+  render() {
+    return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+          <Text>Hello, world 222!</Text>
+        </View>
+    );
+  }
+}
