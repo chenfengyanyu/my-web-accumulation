eac6f9a007b2a08c66dafbfa7aabb063bda5038d
jartto <jartto@qq.com>
2020-02-13 14:22:49 +0800

    add: webp demo

---
+++ webp-demo/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <picture>
+      <source type="image/webp" srcset="./assets/logo.webp">
+      <img src="./assets/logo.png" alt="jartto’s demo">
+    </picture>
+    <div>{{supportFlag}}</div>
+    <HelloWorld msg="Welcome to Jartto's WebP Demo"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Common from './lib/common'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data: ()=> {
+    return {
+      supportFlag: ''
+    }
+  },
+  mounted() {
+    this.checkSupport();
+  },
+  methods: {
+    checkSupport: function() {
+      let self = this;
+      Common.checkWebpFeature('lossy', (feature, isSupported) => {
+        // console.log('>>>>>>>>>>>>',self);
+          if (isSupported) {
+            self.supportFlag = '当前浏览器支持 WebP 格式！';
+          } else {
+            self.supportFlag = 'Sorry，当前浏览器不支持 WebP 格式';
+          }
+      });
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ webp-demo/src/lib/common.js
@@ -0,0 +1,19 @@
+export default {
+    checkWebpFeature: (feature, callback) => {
+        const kTestImages = {
+            lossy: 'UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA',
+            lossless: 'UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==',
+            alpha: 'UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAwAAAARBxAR/Q9ERP8DAABWUDggGAAAABQBAJ0BKgEAAQAAAP4AAA3AAP7mtQAAAA==',
+            animation: 'UklGRlIAAABXRUJQVlA4WAoAAAASAAAAAAAAAAAAQU5JTQYAAAD/////AABBTk1GJgAAAAAAAAAAAAAAAAAAAGQAAABWUDhMDQAAAC8AAAAQBxAREYiI/gcA'
+        };
+        let img = new Image();
+        img.onload = function () {
+            const result = (img.width > 0) && (img.height > 0);
+            callback(feature, result);
+        };
+        img.onerror = function () {
+            callback(feature, false);
+        };
+        img.src = 'data:image/webp;base64,' + kTestImages[feature];
+    }
+}
