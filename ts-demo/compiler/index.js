94005c6e7f5cdad8d3bfa11ca9fd493c8006c03e
jartto <jartto@qq.com>
2020-04-01 10:54:44 +0800

    add: ts-demo

---
+++ ts-demo/compiler/index.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var Hello = /** @class */ (function () {
+    function Hello() {
+    }
+    return Hello;
+}());
+exports.Hello = Hello;
---
+++ ts-demo/demo.ts
@@ -0,0 +1,37 @@
+// string
+let name: string = 'Jartto';
+console.log(name);
+
+// any
+// let age: any = 18;
+// age = 'man';
+
+// number, boolean
+let age: number = 18;
+let man: boolean = true;
+
+// function
+function test(): void {}
+function test1(name: string): string {
+    return 'Jartto';
+}
+
+// class
+class Person {
+    name: string;
+    age: number;
+}
+
+let zhangsan: Person = new Person();
+zhangsan.name = 'Jartto';
+zhangsan.age = 18;
+
+
+
+
+
+
+
+
+
+export {}
